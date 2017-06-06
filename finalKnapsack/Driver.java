5e8686985b74f506e0930d22098af278e5504cc1
Bevin Tang <bevintang@gmail.com>
2017-06-06 01:26:04 -0700

    Completed preliminary features of Driver. Now need to implement algorithms and their solutions.

--- finalKnapsack/Driver.java
+++ finalKnapsack/Driver.java
@@ -19,11 +19,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Driver {
-   private final int ENUM = 0;
-   private final int GREEDY = 1;
-   private final int DYNAMIC = 2;
-   private final int BRANCH = 3;
+   private static final int ENUM = 0;
+   private static final int GREEDY = 1;
+   private static final int DYNAMIC = 2;
+   private static final int BRANCH = 3;
 
+   /*
+      Helper function: used to set the algorithm state and determine the
+                       input filename.
+   */
    private static String[] scanArgs(String[] args){
       String[] data = new String[2];
 
@@ -58,17 +62,72 @@ public class Driver {
       return data;
    }
 
+   /*
+      Helper Function: Retrieves the number of items in the problem from the
+                       input file. If the file is empty, the program will
+                       terminate.
+   */
+   private static int getNumItems(Scanner fileScanner){
+      int numItems = -1;
+      if (fileScanner.hasNext()){
+         numItems = fileScanner.nextInt();
+         fileScanner.nextLine();
+      }
+      else {
+         System.out.println("File is empty");
+         System.exit(1);
+      }
+      return numItems;
+   }
+
+   /*
+      Helper Function: Makes a list of Item objects from the information in
+                       input file.
+   */
+   private static Item[] getItemList(Scanner fileScanner, int numItems) {
+      Item[] itemList = new Item[numItems];
+      for (int i = 0; i < numItems; i++) {
+         // Trim Delimit by all whitespace
+         String[] lineData = fileScanner.nextLine().trim().split("\\s+");
+         int index = Integer.parseInt(lineData[0]);
+         int value = Integer.parseInt(lineData[1]);
+         int weight = Integer.parseInt(lineData[2]);
+         Item item = new Item(index, value, weight);
+         itemList[i] = item;
+      }
+      return itemList;
+   }
+
+   /*
+      Main Function: Organizes data from input file to be used in knapsack
+                     algorithm implementations.
+   */
    public static void main (String[] args) throws FileNotFoundException{
-      String[] data = scanArgs(args);
+      String[] data = scanArgs(args);              // filter arguments
       String filename = data[0];
       int state = Integer.parseInt(data[1]);
-
       File inputFile = new File(filename);
       Scanner fileScanner = new Scanner(inputFile);
+      int numItems = getNumItems(fileScanner);
+      Item[] items = getItemList(fileScanner, numItems);
+      int capacity = fileScanner.nextInt();
+      Item[] solution = new Item[10];
 
-      System.out.printf("State: %d\n", state);
+      /* Run an algorithm determined by the state value */
+      switch (state) {
+         case ENUM:
+            solution = Enumeration.enumerate(items, capacity);
+            break;
+         case GREEDY:
+            break;
+         case DYNAMIC:
+            break;
+         case BRANCH:
+            break;
+      }
 
-      while (fileScanner.hasNext())
-         System.out.println(fileScanner.nextLine());
+      System.out.println(solution[0].getIndex());
+      System.out.println(solution[1].getIndex());
    }
 }
+
--- finalKnapsack/Enumeration.java
+++ finalKnapsack/Enumeration.java
@@ -1,5 +1,8 @@
 public class Enumeration {
-   public void enumerate (){
-   
+   public static Item[] enumerate (Item[] itemList, int capacity){
+      Item[] solution = new Item[2];
+      solution[0] = itemList[0];
+      solution[1] = itemList[1];
+      return solution;
    }
 }
